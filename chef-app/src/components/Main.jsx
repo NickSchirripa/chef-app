8482ee46f49bfa066e59b09d9b2793dd021b8dbe
Nick Schirripa <108542730+NickSchirripa@users.noreply.github.com>
2025-04-21 16:56:21 -0400

    learned useState
    learned how to use:
    -useState
    -useState setter function

--- chef-app/src/components/Main.jsx
+++ chef-app/src/components/Main.jsx
@@ -1,14 +1,32 @@
+import {useState} from "react"
+
 export default function Main(){
+
+
+const [ingredients, setIngrediants] = useState([])
+
+const ingrediantList = ingredients.map(items=> <li>{items}</li>)
+
+    function handleSubmit(event){
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        setIngrediants(prevItems=> [...prevItems, newIngredient])
+    }
     return(
         <main>
-            <form className="ingredient-form">
+            <form onSubmit= {handleSubmit} className="ingredient-form">
                 <input 
                 type="text"
                 placeholder="Salt"
                 aria-label="Add ingredient"
+                name= "ingredient"
                 />
                 <button>Add Ingredient</button>
             </form>
+            <ul>
+                {ingrediantList}
+            </ul>
         </main>
     )
 }
